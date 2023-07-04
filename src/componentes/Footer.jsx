76c525dacc6dfc83e59f39d3f894e2a767948b33
Emmanuel Yapura <eyapura96@gmail.com>
2023-07-04 15:53:29 -0300

    modifica el nombre de la app y agrega atributo target a los links

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -5,13 +5,13 @@ export function Footer() {
     return (
         <div className='bg-slate-900 h-40 md:h-36 pb-5 flex flex-col items-center justify-end'>
             <div className='flex justify-center items-center'>
-                <a href="https://www.instagram.com/emma_y7/">
+                <a href="https://www.instagram.com/emma_y7/" target='_blank'>
                 <BsInstagram className='w-8 h-7 text-slate-50 hover:opacity-75'/>
                 </a>
-                <a href="https://github.com/EmmanuelYapura">
+                <a href="https://github.com/EmmanuelYapura" target='_blank'>
                 <BsGithub  className='w-8 h-7 mx-2 text-slate-50 hover:opacity-75'/>
                 </a>
-                <a href="https://www.linkedin.com/in/emmanuelyapura/">
+                <a href="https://www.linkedin.com/in/emmanuelyapura/" target='_blank'>
                 <AiOutlineLinkedin className='w-8 h-10 text-slate-50 hover:opacity-75'/>
                 </a>
             </div>
